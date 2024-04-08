5be3e4d3820685be54b2b3b2759531a7711be6dc
devashish-mondal <dmondal2580@gmail.com>
2024-04-09 00:16:14 +0530

    test env push

--- src/app/shared/services/api/auth-api.service.ts
+++ src/app/shared/services/api/auth-api.service.ts
@@ -3,17 +3,16 @@ import { Injectable, Signal } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AppToken } from '../../models/app-token';
 import { ApiResponse } from '../../models/api-response';
+import { AppConstant } from 'src/app/utils/app-constant';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthApiService {
 
-  private readonly GOOGLE_TOKEN_ENDPOINT = 'http://localhost:8080/login/oauth2/code/google';
-
   constructor(private http: HttpClient) { }
 
   getOauth2Token(code:string, state: string): Observable<ApiResponse<AppToken>> {
-    return this.http.get<ApiResponse<AppToken>>(`${this.GOOGLE_TOKEN_ENDPOINT}?code=${code}&state=${state}`);
+    return this.http.get<ApiResponse<AppToken>>(`${AppConstant.GOOGLE_TOKEN_ENDPOINT}?code=${code}&state=${state}`);
   }
 }
--- src/app/utils/app-constant.ts
+++ src/app/utils/app-constant.ts
@@ -1,3 +1,5 @@
+import { environment } from "src/environments/environment.development";
+
 export class AppConstant {
 
     /* localstorage constant */
@@ -8,5 +10,7 @@ export class AppConstant {
     public static readonly REGISTER_PATH = 'register';
 
     /* Auth URLs */
-    public static readonly GOOGLE_AUTHORIZATION_ENDPOINT = "http://localhost:8080/oauth2/authorization/google";
+    public static readonly GOOGLE_AUTHORIZATION_ENDPOINT = `${environment.baseUrl}/oauth2/authorization/google`;
+    
+    public static readonly GOOGLE_TOKEN_ENDPOINT = `${environment.baseUrl}/login/oauth2/code/google`;
 }
